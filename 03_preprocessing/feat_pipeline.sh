5a5d28267b1fafc2cfc60d1a591c189ab6a9395f
Olaf Borghi <101983453+olafborghi@users.noreply.github.com>
2023-02-15 16:57:51 +0100

    updated files

--- 03_preprocessing/feat_pipeline.sh
+++ 03_preprocessing/feat_pipeline.sh
@@ -8,21 +8,21 @@ umask u+rw,g+rw,a+rw # give group read/write permissions to all new files
 #==========================================
 
 # General path
-# basedir=/mnt/d/Research/01_AON_ACTION # local
-basedir=/mnt/p/userdata/olafb99/shared/03_AON_ACTION # server
+# basedir=/mnt/c/Users/HP/Documents/03_AON_ACTION # local
+basedir="/home/olafb99/mnt/p/userdata/olafb99/shared/03_AON_ACTION" # server
 
 # script directory (where this script is stored)
-scriptsdir=${basedir}/02_scripts
+scriptsdir="${basedir}/02_scripts"
 
 # directory where data is stored
-datadir=${basedir}/01_data
+datadir="${basedir}/01_data"
 
 # create folder for fsf files of each sbj
-mkdir -p ${basedir}/02_scripts/fsf_dump
+mkdir -p "${basedir}/02_scripts/fsf_dump"
 
 # create and set output folder
-mkdir -p ${basedir}/derivatives
-outputdir=${basedir}/derivatives
+mkdir -p "${basedir}/derivatives"
+outputdir="${basedir}/derivatives"
 
 #==========================================
 # Input options
@@ -31,15 +31,16 @@ outputdir=${basedir}/derivatives
 usage() {
 cat <<EOF
 
-Pipeline to to prepare fieldmap and anatomical images and run melodic
+Pipeline to to prepare fieldmap and anatomical images, create fsf files, run feat, and calculate framewise displacement
 
-usage: wrapper_prep_rest <subjname> 
+usage: wrapper_fsl_fmri <subjname> 
   <subjid> : a single subject number, or a list of comma-separated numbers
   <runs>: comma separated list of strings describing the task and/or run number in BIDS format (e.g., AON_run-01 or rest) 
   <tasklist> : comma separated list of tasks
                -prepdata
                -createfsf
                -runfeat
+               -motion
 EOF
 }
 
@@ -54,7 +55,7 @@ tasks=$3
 
 # create task list if set to 'all'
 if [[ "$tasks" == *"all"* ]]; 
-    then steps=(prepdata,createfsf,runfeat)
+    then steps=(prepdata,createfsf,runfeat,motion)
          tasklist="${steps//,/ }"
     else tasklist="${tasks//,/ }"
 fi
@@ -63,7 +64,7 @@ fi
 # Do the work
 # ============================================
 
-echo ""; echo "START: wrapper feat humans"
+echo ""; echo "START: wrapper FSL bet / preparefieldmap / feat / motion_outliers humans"
 
 # start subject loop
 for subjid in $subjlist ; do
@@ -137,7 +138,7 @@ for subjid in $subjlist ; do
 
               # copy template file to fsf dump folder (can also be the subject folder)
 			        cp $scriptsdir/template.fsf $scriptsdir/fsf_dump/feat_${subj}_${run}.fsf
-              echo "$scriptsdir/scriptsdir/template.fsf to $scriptsdir/fsf_dump"
+              echo "$scriptsdir/scriptsdir/template.fsf to $scriptsdir/fsf_dump/feat_${subj}_${run}.fsf"
               
               # substitute information
               # outputdir
@@ -177,8 +178,55 @@ for subjid in $subjlist ; do
 
           ;;
 
-        # end task switching
-        esac
+          #---------------------------------------------------------------------------------
+          # Run fsl_motion_outliers to calculate framewise displacement for each sbj and run
+          #---------------------------------------------------------------------------------
+
+          motion )
+
+            echo "START: Calculating motion outliers"
+
+            echo "Creating an output html file where motion information will be assembled"
+            outhtml="${outputdir}/bold_motion.html"
+
+            for run in $runs; do 
+              
+              echo "Creating motion directory for $subj and $run"
+              mkdir -p "${outputdir}/${subj}/${run}.feat/motion_assess"
+              motiondir="${outputdir}/${subj}/${run}.feat/motion_assess"
+
+              echo "start fsl_motion_outliers for $subj and $run"
+              fsl_motion_outliers -i "${funcdir}/${subj}_task-${run}_bold" -o "${motiondir}/output.txt" --fd --thresh=0.9 -s "${motiondir}/confound.txt" -p "${motiondir}/fd_plot" -v >> "$outhtml"         
+              echo "done calculating fsl_motion_outliers for $subj and $run"
+
+              echo "Putting the fd plot into the outhtml file"
+              fdplot="${motiondir}/fd_plot.png"
+              echo "
+              <html>
+                <head>
+                  <title>FD plot of $subj and $run</title>
+                  </head>
+                  <body>
+                <h1>FD Plot of $subj and $run</h1>
+                <img src="${fdplot}" width='100%'>
+                </body>
+              </html>" >> $outhtml
+
+              # echo "Place empty file in motiondir if no fd is larger than threshold of .9 for a subj"
+              # confoundfile="${motiondir}/confound.txt"
+              # if test -f "$confoundfile"; then
+              #     echo "$FILE exists. Outliers > .9 were detected."
+              #   else
+              #     echo "No outliers > .9, creating an empty file"
+              #     touch "$confoundfile"
+              # fi
+            
+            done
+          
+          ;;
+
+      # end task switching
+      esac
         
   # end loop over tasks      
   done
